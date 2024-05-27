d2927efa96fcd2e972c4e27255ce96c13744cef7
Mohira <mohira@gmail.com>
2024-05-27 13:19:15 +0500

    Tasks

---
+++ java.js
@@ -0,0 +1,53 @@
+var name = prompt("Enter your name");
+console.log("Your name is " + name);
+
+var age = +prompt("Enter your age");
+console.log("Your age is " + age);
+
+var task1 = +prompt("9+11=?")
+if(task1 == 20){
+    alert("Your answer is true " + task1)
+    console.log("Your answer is true " + task1)
+}else{
+    alert("Your answer false " + task1 + " True answer is 20")
+    console.log("Your answer false " + task1 + " True answer is 20")
+}
+
+var task2 = +prompt("15-9=?")
+if(task2 == 6){
+    alert("Your answer is true " + task2)
+    console.log("Your answer is true " + task2)
+}else{
+    alert("Your answer false " + task2 + " True answer is 6")
+    console.log("Your answer false " + task2 + " True answer is 6")
+}
+
+var task3 = +prompt("5*5=?")
+if(task3 == 25){
+    alert("Your answer is true " + task3)
+    console.log("Your answer is true " + task3)
+}else{
+    alert("Your answer false " + task3 + " True answer is 25")
+    console.log("Your answer false " + task3 + " True answer is 25")
+}
+
+var task4 = +prompt("24/6=?")
+if(task4 == 4){
+    alert("Your answer is true " + task4)
+    console.log("Your answer is true " + task4)
+}else{
+    alert("Your answer false " + task4 + " True answer is 4")
+    console.log("Your answer false " + task4 + " True answer is 4")
+}
+
+var task5 = +prompt("7%3=?")
+if(task5 == 1){
+    alert("Your answer is true " + task5)
+    console.log("Your answer is true " + task5)
+}else{
+    alert("Your answer false " + task5 + " True answer is 1")
+    console.log("Your answer false " + task5 + " True answer is 1")
+}
+
+alert("Done!")
+console.log("Done!")
